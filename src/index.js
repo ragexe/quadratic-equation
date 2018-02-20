0c3d05b586aef53c59004522b3b313bd4a1c61e4
ragexe <ragexe@mail.ru>
2018-02-21 00:53:50 +0300

    quadratic-equation/final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+    let result = [];
+    equation = equation
+        .replace(/\s/g, '')
+        .toLowerCase()
+        .split(/\*x\^2|\*x/)
+        .map((elem) => parseInt(elem));
+
+    const D = equation[1] ** 2 - 4 * equation[0] * equation[2];
+
+    result[0] = Math.round((-equation[1] + D ** 0.5) / (2 * equation[0]));
+    result[1] = Math.round((-equation[1] - D ** 0.5) / (2 * equation[0]));
+
+    return result.sort((a,b) => a - b)
+};
